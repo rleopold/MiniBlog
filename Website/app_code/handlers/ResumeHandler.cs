724dfa620ee002da153213a134c601fcf5d3eece
Richard J. Leopold <rjleopold@gmail.com>
2014-08-14 16:21:58 -0500

    fixed a few weird things

---
+++ Website/app_code/handlers/ResumeHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+public class ResumeHandler : IHttpHandler
+{
+    public bool IsReusable
+    {
+        get { return false; }
+    }
+
+    public void ProcessRequest(HttpContext context)
+    {
+        var s = GetJsonResume(context.Server.MapPath("~/App_Data/resume.json"));
+        context.Response.ContentType = "application/json";
+        context.Response.Write(s);
+    }
+
+    private string GetJsonResume(string path)
+    {
+        //get file and return some json OR maybe use azure storage etc. etc.
+        string result;
+
+        using(var reader = new StreamReader(path))
+        {
+            result = reader.ReadToEnd();
+        }
+
+        return result;
+    }
+}
