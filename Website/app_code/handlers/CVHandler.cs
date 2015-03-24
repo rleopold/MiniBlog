934d93a6a7aa439facb8f58d9c5ae0e8b3e4411f
Richard J. Leopold <rjleopold@gmail.com>
2015-03-24 12:15:12 -0500

    added cv handler

---
+++ Website/app_code/handlers/CVHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CVHandler
+/// </summary>
+public class CVHandler : IHttpHandler
+{
+    public bool IsReusable
+    {
+        get { return false; }
+    }
+
+    public void ProcessRequest(HttpContext context)
+    {
+
+        var content = GetCvContent(context.Server.MapPath("~/App_Data/cv.html"));
+        context.Response.ContentType = "text/html";
+
+        context.Response.Write(content);
+    }
+
+    private string GetCvContent(string path)
+    {
+        //get file html 
+        string result;
+
+        using (var reader = new StreamReader(path))
+        {
+            result = reader.ReadToEnd();
+        }
+
+        return result;
+    }
+}
